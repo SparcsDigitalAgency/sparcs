b7ea8d15914df9186bfda61eb75dcdee325cc0f5
amitha-babu <36689295+amitha-babu@users.noreply.github.com>
2018-02-21 14:12:37 +0530

    test

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+<!-- amitha-->
 <meta http_equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name=viewport content="width=device-width, initial-scale=1">
 <title><?php echo $meta_tags['title']; ?></title>
