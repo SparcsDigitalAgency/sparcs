8c7c5f93bad87098cab6fa6d857989c2613ab400
sahid132 <42166973+sahid132@users.noreply.github.com>
2018-09-25 17:39:05 +0530

    commit

--- sparcs-new/company.php
+++ sparcs-new/company.php
@@ -76,6 +76,106 @@
 <!--==========================
    end of About Us section 3
    ============================-->
+
+   <!--==========================
+   About Us section4
+   ============================-->
+<section id="about-section4">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-12">
+            <div class="text-holder wow fadeInDown text-center">
+               <h2>Brainstorm</h2>
+               <p class="text-muted">The first step would be to sit down and get to know you.<br> We'll discuss goals and project objectives and any other relevant information.</p>
+            </div>
+         </div>
+         <div class="col-lg-7" style="margin: 0 auto;">
+            <div class="image-holder wow fadeInUp">
+               <img src="img/about2.png" class="img-responsive">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--==========================
+   end of About Us section 4
+   ============================-->
+      <!--==========================
+   About Us section5
+   ============================-->
+<section id="about-section5">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-12">
+            <div class="text-holder wow fadeInDown text-center">
+               <h2>Planning</h2>
+               <p class="text-muted">With the information gathered from our brainstorm session, <br> wireframes would be developed defining layout, interactions and content structure.</p>
+            </div>
+         </div>
+         <div class="col-lg-7" style="margin: 0 auto;">
+            <div class="image-holder wow fadeInUp">
+               <img src="img/about3.png" class="img-responsive">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--==========================
+   end of About Us section 5
+   ============================-->
+
+   <!--==========================
+   About Us section6
+   ============================-->
+<section id="about-section6">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6">
+            <div class="text-holder wow fadeInLeft">
+               <h2>Designing</h2>
+               <p class="text-muted">Once the wireframes are finalized, detailed mock-ups <br>of key pages will be designed to show the look and<br> feel of your product.</p>
+            </div>
+         </div>
+         <div class="col-lg-6">
+            <div class="img-holder">
+               <img src="img/about4.png" class="img-responsive wow fadeInRight">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--==========================
+   end of About Us section 6
+   ============================-->
+   <!--==========================
+   end of About Us section 5
+   ============================-->
+
+   <!--==========================
+   About Us section7
+   ============================-->
+<section id="about-section7">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6">
+            <div class="img-holder">
+               <img src="img/about-5.png" class="img-responsive wow fadeInLeft">
+            </div>
+         </div>
+         <div class="col-lg-6">
+            <div class="text-holder wow fadeInRight ">
+               <h2>Development</h2>
+               <p class="text-muted">With the final creative concepts completed, the designs are then coded and transformed into a fully functional product.</p>
+            </div>
+         </div>
+         
+      </div>
+   </div>
+</section>
+<!--==========================
+   end of About Us section 7
+   ============================-->
+
 <!--==========================
 contact-us Section
 ============================-->
