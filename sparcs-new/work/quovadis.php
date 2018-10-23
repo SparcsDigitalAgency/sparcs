05cdbe9a45a734048cf14f2a4fad0935b5c417a8
sahid132 <42166973+sahid132@users.noreply.github.com>
2018-10-23 15:32:45 +0530

    Update sparcs-new/work/quovadis.php

--- sparcs-new/work/quovadis.php
+++ sparcs-new/work/quovadis.php
@@ -26,7 +26,7 @@ include_once('../partials/header.php')
  <!--==========================
    portfolio  Section1
    ============================-->
-<section id="portfolio-section1" style="background:#c1c1c1;">
+<section id="portfolio-section1" style="background:#eee;">
    <div class="container-fluid cf-padding">
       <div class="row">
          <div class="col-lg-6"  style="padding: 50px 0px 0px 0px;">
