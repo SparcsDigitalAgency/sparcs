5d4332a61e37c1b3db426398410be445f069ca53
sahid132 <42166973+sahid132@users.noreply.github.com>
2018-11-01 18:09:31 +0530

    esmart

---
+++ sparcs-new/work/esmart.php
@@ -0,0 +1,151 @@
+<!-- header styles-->
+<?php 
+   $meta_tags=array(
+            'title'        => 'Sparcs Digital Agency | Digital Agency in Kochi, Kerala',
+         'description'  => 'Sparcs is a Digital Agency which delivers high quality, reliable and affordable services to our customer globally.',
+         'keywords'=>'web development, web design, branding, digital marketing, SEO, kochi, Kerala',
+         "no-index" => true,
+         'no-follow' =>true,
+         'no-archieve' =>true,
+   
+      );
+   
+   include_once('../partials/header.php')
+   ?>
+
+<!--==========================#
+   portfolio  Section1
+   ============================-->
+<section id="genex-portfolio-section1" class="dark-section">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6 paddingtop50only">
+            <div class="portfolio-section-imgholder text-center">
+               <img src="<?php echo $path; ?>img/genex/genex-responsive.png" alt="" class="img-responsive wow fadeInLeft">
+            </div>
+         </div>
+         <div class="col-lg-6 paddingsides80">
+            <div class="portfolio-section-textholder">
+               <img src="<?php echo $path; ?>img/genex/genex-logo-w.png" alt="" class="img-responsive wow fadeInLeft">
+               <p class="wow fadeInRight ">Branding and Web Development with visually appealing modern design for GenX, a Security Products Company with a wide range of product options.</p>
+            </div>
+            <div class="portfolio-section-textholder wow fadeInRight">
+               <div class="icon"><i class="fa fa-check-circle"></i></div>
+               <p class="description">Designed and Developed a Responsive Website  based on Wordpress framework with frontend technology matching top notch industry standards.</p>
+            </div>
+            <div class="portfolio-section-textholder wow fadeInRight">
+               <div class="icon"><i class="fa fa-check-circle"></i></div>
+               <p class="description">Converting static pages to Content Management System(CMS) by which Client is enabled to keep adding new products from backend.</p>
+            </div>
+            <div class="portfolio-section-textholder wow fadeInRight">
+               <div class="icon"><i class="fa fa-check-circle"></i></div>
+               <p class="description">Content writing aimed at drawing consumer attention to products.</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--==========================#
+   end of portfolio  Section1
+   ============================-->
+
+<!--==========================
+   portfolio section5
+   ============================-->
+<section id="genex-portfolio-section5" style="margin-top: 100px;">
+   <div class="container no-padding">
+      <div class="row no-gutters">
+         <div class="col-lg-6 bg-red">
+            <div class="text-holder wow fadeIn text-center">
+               <h2>Brochure</h2>
+               <p>When Gen X diversified into Smart Homes Technological Applications it was new in the local market.We created brochures to popularise and sell the concept.</p>
+            </div>
+         </div>
+         <div class="col-lg-6">
+            <div class="img-holder auto-margin wow fadeIn">
+               <img src="<?php echo $path; ?>img/esmart/1.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section id="genex-portfolio-section5">
+   <div class="container no-padding">
+      <div class="row no-gutters">
+         <div class="col-lg-6">
+            <div class="img-holder auto-margin wow fadeIn">
+               <img src="<?php echo $path; ?>img/esmart/2.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+         <div class="col-lg-6 bg-red">
+            <div class="text-holder wow fadeIn text-center">
+               <h2>Brochure</h2>
+               <p>When Gen X diversified into Smart Homes Technological Applications it was new in the local market.We created brochures to popularise and sell the concept.</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section id="genex-portfolio-section5">
+   <div class="container no-padding">
+      <div class="row no-gutters">
+         <div class="col-lg-6 bg-red">
+            <div class="text-holder wow fadeIn text-center">
+               <h2>Brochure</h2>
+               <p>When Gen X diversified into Smart Homes Technological Applications it was new in the local market.We created brochures to popularise and sell the concept.</p>
+            </div>
+         </div>
+         <div class="col-lg-6">
+            <div class="img-holder auto-margin wow fadeIn">
+               <img src="<?php echo $path; ?>img/esmart/3.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section id="genex-portfolio-section5">
+   <div class="container no-padding">
+      <div class="row no-gutters">
+         <div class="col-lg-6">
+            <div class="img-holder auto-margin wow fadeIn">
+               <img src="<?php echo $path; ?>img/esmart/4.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+         <div class="col-lg-6 bg-red">
+            <div class="text-holder wow fadeIn text-center">
+               <h2>Brochure</h2>
+               <p>When Gen X diversified into Smart Homes Technological Applications it was new in the local market.We created brochures to popularise and sell the concept.</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section id="genex-portfolio-section5" style="margin-bottom: 100px;">
+   <div class="container no-padding">
+      <div class="row no-gutters">
+         <div class="col-lg-6 bg-red">
+            <div class="text-holder wow fadeIn text-center">
+               <h2>Brochure</h2>
+               <p>When Gen X diversified into Smart Homes Technological Applications it was new in the local market.We created brochures to popularise and sell the concept.</p>
+            </div>
+         </div>
+         <div class="col-lg-6">
+            <div class="img-holder auto-margin wow fadeIn">
+               <img src="<?php echo $path; ?>img/esmart/5.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--==========================
+   end of portfolio section5
+   ============================-->
+ 
+
+
+<!--==========================
+   Footer
+   ============================-->
+<?php include_once('../partials/footer.php')?>
+<!-- end footer -->
+
