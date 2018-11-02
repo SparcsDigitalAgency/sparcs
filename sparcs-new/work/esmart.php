c5ff495dbbc659862e62a75c2716763a24b870da
sahid132 <42166973+sahid132@users.noreply.github.com>
2018-11-02 14:04:24 +0530

    esmart

--- sparcs-new/work/esmart.php
+++ sparcs-new/work/esmart.php
@@ -16,31 +16,15 @@
 <!--==========================#
    portfolio  Section1
    ============================-->
-<section id="genex-portfolio-section1" class="dark-section">
+<section id="esmart-portfolio-section1">
    <div class="container">
       <div class="row">
-         <div class="col-lg-6 paddingtop50only">
-            <div class="portfolio-section-imgholder text-center">
-               <img src="<?php echo $path; ?>img/genex/genex-responsive.png" alt="" class="img-responsive wow fadeInLeft">
-            </div>
+         <div class="text-holder auto-margin wow fadeInDown">
+            <h2>Logo Designing</h2>
+            <p> The inspiring story of a daring entrepreneurship that was looking <br>for a website revamp to reflect its Global Aspirations.</p>
          </div>
-         <div class="col-lg-6 paddingsides80">
-            <div class="portfolio-section-textholder">
-               <img src="<?php echo $path; ?>img/genex/genex-logo-w.png" alt="" class="img-responsive wow fadeInLeft">
-               <p class="wow fadeInRight ">Branding and Web Development with visually appealing modern design for GenX, a Security Products Company with a wide range of product options.</p>
-            </div>
-            <div class="portfolio-section-textholder wow fadeInRight">
-               <div class="icon"><i class="fa fa-check-circle"></i></div>
-               <p class="description">Designed and Developed a Responsive Website  based on Wordpress framework with frontend technology matching top notch industry standards.</p>
-            </div>
-            <div class="portfolio-section-textholder wow fadeInRight">
-               <div class="icon"><i class="fa fa-check-circle"></i></div>
-               <p class="description">Converting static pages to Content Management System(CMS) by which Client is enabled to keep adding new products from backend.</p>
-            </div>
-            <div class="portfolio-section-textholder wow fadeInRight">
-               <div class="icon"><i class="fa fa-check-circle"></i></div>
-               <p class="description">Content writing aimed at drawing consumer attention to products.</p>
-            </div>
+         <div class="img-holder auto-margin">
+            <img src="<?php echo $path; ?>img/esmart/esmart-logo.png" alt="" class="style2-pic img-responsive wow fadeInDown" data-wow-duration="2s">
          </div>
       </div>
    </div>
@@ -120,7 +104,7 @@
       </div>
    </div>
 </section>
-<section id="genex-portfolio-section5" style="margin-bottom: 100px;">
+<section id="genex-portfolio-section5">
    <div class="container no-padding">
       <div class="row no-gutters">
          <div class="col-lg-6 bg-red">
@@ -132,6 +116,23 @@
          <div class="col-lg-6">
             <div class="img-holder auto-margin wow fadeIn">
                <img src="<?php echo $path; ?>img/esmart/5.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<section id="genex-portfolio-section5"  style="margin-bottom: 100px;">
+   <div class="container no-padding">
+      <div class="row no-gutters">
+         <div class="col-lg-6">
+            <div class="img-holder auto-margin wow fadeIn">
+               <img src="<?php echo $path; ?>img/esmart/6.jpg" alt="" class="img-responsive wow fadeIn" style="">
+            </div>
+         </div>
+         <div class="col-lg-6 bg-red">
+            <div class="text-holder wow fadeIn text-center">
+               <h2>Brochure</h2>
+               <p>When Gen X diversified into Smart Homes Technological Applications it was new in the local market.We created brochures to popularise and sell the concept.</p>
             </div>
          </div>
       </div>
